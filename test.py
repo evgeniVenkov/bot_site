d60e26a513f59f54a62dc08999520625b6c4a941
evgeniVenkov <evgenijvenkov183@gmail.com>
2025-06-04 22:16:10 +0300

    решал задачу из интернета

--- test.py
+++ test.py
@@ -1,10 +1,23 @@
+import random
+def task1():
+    mass = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+    num = int(input("Enter a number: "))
 
 
-mass = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    sdvig = len(mass)-num
+    sdvig = mass[sdvig:] + mass[:sdvig]
+    print(sdvig)
 
-num = int(input("Enter a number: "))
+def task2():
+    num_rand = random.randint(1,10000)
+    num = num_rand
+    while True:
+        num_rand = num_rand /10
+        if num_rand < 10:
+            print(int(num_rand))
+            print(num)
+            break
 
 
-sdvig = len(mass)-num
-sdvig = mass[sdvig:] + mass[:sdvig]
-print(sdvig)
+task2()
