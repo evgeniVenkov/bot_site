279b168061dec53456ae6d0ee3b661fbd9e756fb
evgeniVenkov <evgenijvenkov183@gmail.com>
2025-06-08 17:28:47 +0300

    решал задачу из интернета

--- test.py
+++ test.py
@@ -49,5 +49,17 @@ def task7():
     mass = [1,-2,3,4,-5,6,7,-8,9,10,-11,12,13,14,15]
     new_mass = [i for i in mass if i < 0]
     print(len(new_mass))
+def task8():
+    text = "Иногда достаточно просто остановиться. Не чтобы сдаться, а чтобы оглянуться. Мир не уходит вперёд без тебя — он ждёт, когда ты заметишь, как красиво свет падает сквозь листья, как тихо дышит утро, и как многое можно почувствовать, ничего не говоря."
+    split = text.split(" ")
+    count = 0
+    mass_word = []
+    for el in split:
+        if el[0].lower() == "а":
+            count += 1
+            mass_word.append(el)
 
-task7()
+    print(count)
+    print(mass_word)
+
+task8()
