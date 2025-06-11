540e554bd42dd223cc0bee5505211b0fded0a5b8
evgeniVenkov <evgenijvenkov183@gmail.com>
2025-06-11 17:58:34 +0300

    решал задачу из интеренета

--- test.py
+++ test.py
@@ -87,4 +87,51 @@ def task9():
         mass.append(el)
 
     print(mass)
-task9()
+
+def task10():
+    dct = {
+        1: {
+            1: {
+                1: 111,
+                2: 112,
+                3: 113,
+            },
+            2: {
+                1: 121,
+                2: 122,
+                3: 123,
+            },
+        },
+        2: {
+            1: {
+                1: 211,
+                2: 212,
+                3: 213,
+            },
+            2: {
+                1: 221,
+                2: 222,
+                3: 223,
+            },
+        },
+        3: {
+            1: {
+                1: 311,
+                2: 312,
+                3: 313,
+            },
+            2: {
+                1: 321,
+                2: 322,
+                3: 323,
+            },
+        },
+    }
+    count = 0
+    for el in dct.values():
+        for i in el.values():
+            for j in i.values():
+                count += int(j)
+    print(count)
+
+task10()
